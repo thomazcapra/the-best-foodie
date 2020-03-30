4202b14cd3cc5602608a8a3e0a3f3990ffb7cd95
Thomaz Capra <thomaz.capra@millenniumbcp.pt>
2020-03-30 20:33:44 +0100

    Added maintext and logo stamp

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -3,6 +3,8 @@ import { Section } from 'components';
 import { SectionsData } from 'sections/sections.models';
 import backgroundImage from 'assets/images/background.png';
 
+import logo from 'assets/images/stamp.png';
+
 export const About = (): JSX.Element => (
   <Section
     backgroundColor={'#FFDAC9'}
@@ -17,6 +19,60 @@ export const About = (): JSX.Element => (
             height: '100%'
           }}
         />
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              position: 'relative'
+            }}
+          >
+            <img
+              style={{
+                position: 'absolute',
+                left: '-200px',
+                top: '-220px'
+              }}
+              src={logo}
+              alt={logo}
+            />
+            <div
+              style={{
+                fontSize: '82px',
+                lineHeight: '95px'
+              }}
+            >
+              THE BEST FOODIE
+            </div>
+            <div
+              style={{
+                fontSize: '115px',
+                lineHeight: '95px'
+              }}
+            >
+              EXPERIENCE
+            </div>
+            <div
+              style={{
+                fontSize: '35px',
+                lineHeight: '52px'
+              }}
+            >
+              NOW IN LONDON
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     }
   />
